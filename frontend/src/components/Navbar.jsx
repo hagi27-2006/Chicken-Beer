b18e40be8d947b1aecb36d38ea4dfe91b0bf1477
tugu <tugit8833@gmail.com>
2025-06-05 15:58:12 +0800

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Navbar() {
   return (
-    <nav className="bg-white h-2xl shadow-md w-full">
+    <nav className="font-manrope bg-[#F9F9F9] h-2xl shadow-md w-full">
       <div className="max-w-xl mx-auto px-4 py-3 flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-red-600">KFC</Link>
+        <Link to="/">
+          <img src={logo} alt="Logo" className="h-14" />
+        </Link>
+
         <div className="space-x-6">
-          <Link to="/menu" className="text-gray-700 hover:text-red-600">Меню</Link>
-          <Link to="/bar" className="text-gray-700 hover:text-red-600">Салбар</Link>
-          <Link to="/delivery" className="text-gray-700 hover:text-red-600">Захиалгын явц</Link>
-          <Link to="/login" className="text-gray-700 hover:text-red-600">Нэвтрэх</Link>
+          <Link to="/" className="text-gray-700 hover:text-red-600">
+            Меню
+          </Link>
+          <Link to="/bar" className="text-gray-700 hover:text-red-600">
+            Салбар
+          </Link>
+          <Link to="/delivery" className="text-gray-700 hover:text-red-600">
+            Захиалгын явц
+          </Link>
+          <Link to="/login" className="text-gray-700 hover:text-red-600">
+            Нэвтрэх
+          </Link>
         </div>
       </div>
     </nav>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        manrope: ['Manrope', 'sans-serif'],
+      },
+      keyframes: {
+        'fade-slide': {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-slide': 'fade-slide 0.6s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
